6eea120d1e46061886ebdb0b3d85e6de2a3dce34
Maria Gorozhanova <zvezdoz88@gmail.com>
2022-12-11 11:29:33 +0300

    Add logo to LogInView

--- Navigation/Profile/LogInViewController.swift
+++ Navigation/Profile/LogInViewController.swift
@@ -8,6 +8,12 @@
 import UIKit
 
 class LogInViewController: UIViewController {
+    
+    lazy var logoView: UIImageView = {
+        let view = UIImageView(image: UIImage(named: "logo"))
+        
+        return view
+    }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -18,5 +24,20 @@ class LogInViewController: UIViewController {
     func setupView() {
         self.navigationController?.navigationBar.isHidden = true
         self.view.backgroundColor = .white
+        
+        self.view.addSubview(logoView)
+        
+        applyConstraints()
+    }
+    
+    func applyConstraints() {
+        logoView.translatesAutoresizingMaskIntoConstraints = false
+        
+        NSLayoutConstraint.activate([
+            logoView.topAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.topAnchor, constant: 120),
+            logoView.widthAnchor.constraint(equalToConstant: 100),
+            logoView.heightAnchor.constraint(equalToConstant: 100),
+            logoView.centerXAnchor.constraint(equalTo: self.view.centerXAnchor)
+        ])
     }
 }
