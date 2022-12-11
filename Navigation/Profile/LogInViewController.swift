683e8bcf988b54305ec063aebd73e45d2a4c432c
Maria Gorozhanova <zvezdoz88@gmail.com>
2022-12-11 12:24:09 +0300

    Add text field for password

--- Navigation/Profile/LogInViewController.swift
+++ Navigation/Profile/LogInViewController.swift
@@ -16,23 +16,20 @@ class LogInViewController: UIViewController {
     }()
     
     lazy var loginTextField: UITextField = {
-        let textField = UITextField()
+        let textField = createStyledTextField()
         
         textField.placeholder = "Email or phone"
-        textField.layer.borderColor = UIColor.lightGray.cgColor
-        textField.layer.borderWidth = 0.5
-        textField.layer.cornerRadius = 10
         textField.layer.maskedCorners = [.layerMinXMinYCorner, .layerMaxXMinYCorner]
-        textField.textColor = .black
-        textField.font = UIFont.systemFont(ofSize: 16, weight: .regular)
-        textField.tintColor = UIColor(named: "TintColor")
-        textField.backgroundColor = .systemGray6
-        textField.autocapitalizationType = .none
-        textField.autocorrectionType = .no
         
-        let spaceView = UIView(frame: CGRect(x: 0, y: 0, width: 12, height: 0))
-        textField.leftView = spaceView
-        textField.leftViewMode = .always
+        return textField
+    }()
+    
+    lazy var passwordTextField: UITextField = {
+        let textField = createStyledTextField()
+        
+        textField.placeholder = "Password"
+        textField.layer.maskedCorners = [.layerMinXMaxYCorner, .layerMaxXMaxYCorner]
+        textField.isSecureTextEntry = true
         
         return textField
     }()
@@ -49,13 +46,35 @@ class LogInViewController: UIViewController {
         
         self.view.addSubview(logoView)
         self.view.addSubview(loginTextField)
+        self.view.addSubview(passwordTextField)
         
         applyConstraints()
+    }
+    
+    func createStyledTextField() -> UITextField {
+        let textField = UITextField()
+
+        textField.layer.borderColor = UIColor.lightGray.cgColor
+        textField.layer.borderWidth = 0.5
+        textField.layer.cornerRadius = 10
+        textField.textColor = .black
+        textField.font = UIFont.systemFont(ofSize: 16, weight: .regular)
+        textField.tintColor = UIColor(named: "TintColor")
+        textField.backgroundColor = .systemGray6
+        textField.autocapitalizationType = .none
+        textField.autocorrectionType = .no
+        
+        let spaceView = UIView(frame: CGRect(x: 0, y: 0, width: 12, height: 0))
+        textField.leftView = spaceView
+        textField.leftViewMode = .always
+        
+        return textField
     }
     
     func applyConstraints() {
         logoView.translatesAutoresizingMaskIntoConstraints = false
         loginTextField.translatesAutoresizingMaskIntoConstraints = false
+        passwordTextField.translatesAutoresizingMaskIntoConstraints = false
         
         NSLayoutConstraint.activate([
             logoView.topAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.topAnchor, constant: 120),
@@ -66,7 +85,12 @@ class LogInViewController: UIViewController {
             loginTextField.topAnchor.constraint(equalTo: logoView.bottomAnchor, constant: 120),
             loginTextField.leadingAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.leadingAnchor, constant: 16),
             loginTextField.trailingAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.trailingAnchor, constant: -16),
-            loginTextField.heightAnchor.constraint(equalToConstant: 50)
+            loginTextField.heightAnchor.constraint(equalToConstant: 50),
+            
+            passwordTextField.topAnchor.constraint(equalTo: loginTextField.bottomAnchor, constant: -1),
+            passwordTextField.leadingAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.leadingAnchor, constant: 16),
+            passwordTextField.trailingAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.trailingAnchor, constant: -16),
+            passwordTextField.heightAnchor.constraint(equalToConstant: 50),
         ])
     }
 }
